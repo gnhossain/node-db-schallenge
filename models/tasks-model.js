4454ba9a198a8aa22f130414811ae73ea8f74ef8
gul hossain <spicymangojungle@gmail.com>
2020-04-17 15:08:35 -0700

    project name and desc

--- models/tasks-model.js
+++ models/tasks-model.js
@@ -7,12 +7,15 @@ module.exports = {
 }
 
 function getTasks() {
-    return db("tasks")
+    return db("tasks as t")
+        .join("projects as p", "t.project_id", "p.id")
+        .select("t.id","t.description", "t.notes", "t.completed", "p.name as project_name", "p.description as project_description")
+
 }
 
 function getTaskById(id) {
     return db("tasks as t")
-    .where({id})
+        .where({id})
 }
 
 function addTask(task){
